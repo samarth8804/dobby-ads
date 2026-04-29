7d9bfb2907a81d00f2ce033d83872baa108caa58
samarth8804 <samarthmittal0808@gmail.com>
2026-04-29 22:53:24 +0530

    Refactor app.js to include folder and authentication routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,26 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import folderRoutes from "./routes/folder.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/folder", folderRoutes);
 
 export default app;
