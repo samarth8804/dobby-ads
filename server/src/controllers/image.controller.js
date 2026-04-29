65e8001e55384e786ed249bd33a2c0b0c3962750
samarth8804 <samarthmittal0808@gmail.com>
2026-04-30 01:39:09 +0530

    Add image deletion functionality and update folder size on image upload

--- server/src/controllers/image.controller.js
+++ server/src/controllers/image.controller.js
@@ -1,11 +1,16 @@
 import cloudinary from "../config/cloudinary.js";
 import Image from "../models/Image.model.js";
 import Folder from "../models/Folder.model.js";
+import { updateFolderSize } from "../utils/folderSize.util.js";
+import { getRootFolder } from "../utils/folder.util.js";
 
 export const uploadImage = async (req, res) => {
   try {
     const file = req.file;
-    const { folderId } = req.body;
+    const userId = req.user.id;
+    const root = await getRootFolder(userId);
+
+    const folderId = req.body.folderId || root._id;
 
     if (!file) {
       return res.status(400).json({ message: "No file uploaded" });
@@ -44,8 +49,50 @@ export const uploadImage = async (req, res) => {
       userId: req.user.id,
     });
 
+    await updateFolderSize(folderId, file.size, req.user.id);
+
     res.status(201).json(image);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.user.id;
+
+    // Find image
+    const image = await Image.findOne({
+      _id: id,
+      userId,
+    });
+
+    if (!image) {
+      return res.status(404).json({
+        message: "Image not found",
+      });
+    }
+
+    // Delete from Cloudinary
+    try {
+      await cloudinary.uploader.destroy(image.publicId);
+    } catch (err) {
+      console.log("Cloudinary delete failed");
+    }
+
+    // Delete from DB
+    await Image.findByIdAndDelete(id);
+
+    // Update folder size
+    await updateFolderSize(image.folderId, -image.size, userId);
+
+    res.json({
+      message: "Image deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
   }
 };
--- server/src/routes/image.route.js
+++ server/src/routes/image.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { uploadImage } from "../controllers/image.controller.js";
+import { uploadImage, deleteImage } from "../controllers/image.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, upload.single("image"), uploadImage);
+router.delete("/:id", protect, deleteImage);
 
 export default router;
