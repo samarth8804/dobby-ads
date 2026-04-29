b985ccfb842f97a124238778c42f1a3813fff53e
samarth8804 <samarthmittal0808@gmail.com>
2026-04-29 22:52:53 +0530

    Add authentication routes for signup, login, refresh token, and logout

---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  signup,
+  login,
+  refreshToken,
+  logout,
+} from "../controllers/auth.controller.js";
+import {
+  signupValidator,
+  loginValidator,
+} from "../validators/auth.validators.js";
+import { protect } from "../middlewares/auth.middleware.js";
+
+import { validate } from "../middlewares/validate.middleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signupValidator, validate, signup);
+router.post("/login", loginValidator, validate, login);
+router.post("/refresh", refreshToken);
+router.post("/logout", protect, logout);
+router.get("/me", protect, (req, res) => {
+  res.json({ user: req.user });
+});
+
+export default router;
