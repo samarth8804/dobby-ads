9adf2fb7b0f064c9db80da490c6be818f9ad92b3
samarth8804 <samarthmittal0808@gmail.com>
2026-04-29 22:52:46 +0530

    Add authentication middleware to protect routes with JWT verification

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded; // contains user id
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
