242f0f4503cf665cdc05a173a958a55b20dccf6d
samarth8804 <samarthmittal0808@gmail.com>
2026-04-29 17:54:58 +0530

    Add database connection setup with mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
