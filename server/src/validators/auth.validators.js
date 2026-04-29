b4af3ac225ce419e859072090eab5739493a0302
samarth8804 <samarthmittal0808@gmail.com>
2026-04-29 22:52:38 +0530

    Add validation middleware and authentication validators for signup and login

---
+++ server/src/validators/auth.validators.js
@@ -0,0 +1,38 @@
+import { body } from "express-validator";
+
+export const signupValidator = [
+  body("username")
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters"),
+
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email format")
+    .normalizeEmail(),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters")
+    .matches(/^(?=.*[A-Z])(?=.*\d)/)
+    .withMessage(
+      "Password must contain at least one uppercase letter and one number"
+    ),
+];
+
+export const loginValidator = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email format"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required"),
+];
