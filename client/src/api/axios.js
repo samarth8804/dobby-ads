df7f34121b2d05848a7e206fbf1e4bbfbe02c755
samarth8804 <samarthmittal0808@gmail.com>
2026-04-30 23:18:06 +0530

    Fix import statement for Redux store in axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import store from "../redux/store";
+import { store } from "../redux/store";
 import { setAccessToken, clearAuth } from "../redux/slices/authSlice";
 import * as authApi from "./auth.api";
 
